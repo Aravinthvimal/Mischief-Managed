5a6b053dcd89683a23de148abe179e2d634604fc
Aravinth-Vimal-08 <aravinthvimal08@gmail.com>
2022-08-31 23:03:55 +0530

    Signin signout added

--- server/API/Auth/index.js
+++ server/API/Auth/index.js
@@ -45,6 +45,51 @@ function generateAccessToken(user) {
     return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn : '7d'} ) 
 }
 
+/*
+Route         /signin
+Descrip       Signin with email and password
+Params        None
+Access        Public
+Method        POST
+*/
 
+Router.post("/signin", async(req,res) => {
+    try {
+      
+      await ValidateSignin(req.body.credentials);
+      const new_user = await UserModel.findByEmailAndPassword(req.body.credentials);
+      const user = { email : new_user }
+  
+      //JWT Auth Token
+      const token = generateAccessToken(user)
+      const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+      refreshTokens.push(refreshToken);
+  
+      return res.status(200).json({token, refreshToken, status: "Success"});
+  
+    } catch (error) {
+      return res.status(500).json({error: error.message});
+    }
+});
+
+/*
+Route         /signout
+Descrip       Signout with email and password
+Params        None
+Access        Public
+Method        POST
+*/
+
+Router.delete("/signout", async(req, res) => {
+    try {
+      const refreshToken = req.body.token
+  
+      refreshTokens = refreshTokens.filter(token => token !== refreshToken)
+      res.status(204).json({ message : "logged out" })
+  
+    } catch (error) {
+      return res.status(500).json({ error : error.message })
+    }
+});
 
 export default Router;
--- server/database/users/index.js
+++ server/database/users/index.js
@@ -53,6 +53,7 @@ UserSchema.pre("save", function(next){
     //generating bcrypt salt
     bcrypt.genSalt(8,(error,salt)=> {
         if(error) return next(error);
+
             //hashing the password
             bcrypt.hash(user.password, salt, (error,hash)=> {
             if(error) return next(error);
@@ -60,6 +61,7 @@ UserSchema.pre("save", function(next){
             //assigning hashed password
             user.password = hash;
             return next();
+            
         });
     });
 });
